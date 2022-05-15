dd70bd8357a02990b8d234d021c03823e3fc2217
Алёна <105580854+alyonadombrina@users.noreply.github.com>
2022-05-15 10:59:30 +0300

    Add files via upload

---
+++ ConsoleApplication27.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{ 
+	for (int i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0) cout << "Fizz"<<endl;
+		if (i % 5 == 0) cout << "Buzz"<<endl;
+		if (i % 3 == 0 && i % 5 == 0) cout << "FizzBuzz"<<endl;
+		else cout << i<<endl;
+	}
+    return 0;
+}
+
